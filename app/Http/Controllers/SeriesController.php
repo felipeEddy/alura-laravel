2f582b21423530a13501a236a0c8a44f82561452
Felipe Eduardo <felipe.eddy2@hotmail.com>
2022-08-12 23:55:26 -0300

    initial commit

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Serie;
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
+{
+    public function index()
+    {
+        $series = Serie::query()->orderBy('nome')->get();
+
+        return view('series.index', compact('series'));
+    }
+
+    public function create()
+    {
+        return view('series.create');
+    }
+
+    public function store(Request $request)
+    {
+        $serie = new Serie();
+        $serie->nome = $request->input('nome');
+        $serie->save();
+
+        return redirect('/series');
+    }
+}
